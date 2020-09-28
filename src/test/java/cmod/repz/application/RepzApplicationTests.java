a17716dd1a94112c1e45637420c56603017cced3
sepehr-gh <sepehrgh01@gmail.com>
2020-09-28 09:48:32 +0330

    Initial Commit

---
+++ src/test/java/cmod/repz/application/RepzApplicationTests.java
@@ -0,0 +1,13 @@
+package cmod.repz.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
