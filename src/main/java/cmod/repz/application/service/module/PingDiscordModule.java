875f9f44f1c46e9c477ddd3211f3791baa4578f1
sepehr-gh <sepehrgh01@gmail.com>
2020-10-01 13:15:43 +0330

    Fixed message sending issue

--- src/main/java/cmod/repz/application/service/module/PingDiscordModule.java
+++ src/main/java/cmod/repz/application/service/module/PingDiscordModule.java
@@ -5,13 +5,15 @@ import cmod.repz.application.service.listener.DiscordCommandListener;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.util.Arrays;
+
 @DiscordListenerComponent(command = "repz")
 @Slf4j
 public class PingDiscordModule implements DiscordCommandListener {
 
     @Override
     public void onCommand(GuildMessageReceivedEvent event, String[] args) {
-        log.info("Discord message arrived:" + args);
-        event.getMessage().getChannel().sendMessage("I'm up! thanks for checking!");
+        log.info("Discord message arrived:" + Arrays.toString(args));
+        event.getMessage().getChannel().sendMessage("I'm up! thanks for checking!").complete();
     }
 }
