36513a27cad29d19a73d95f0f05180d97962a4d8
sepehr-gh <sepehrgh01@gmail.com>
2020-10-06 17:26:42 +0330

    Removed unused imports

--- src/main/java/cmod/repz/application/service/module/CompleteRegisterComponent.java
+++ src/main/java/cmod/repz/application/service/module/CompleteRegisterComponent.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.Objects;
 
 @Component
 @Slf4j
