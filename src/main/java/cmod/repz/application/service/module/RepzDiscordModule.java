d218a2ff6a18cb598687bb42a17c936f1fa83bc2
sepehr-gh <sepehrgh01@gmail.com>
2020-10-16 13:28:08 +0330

    Removing bot response to !repz after 120 seconds

--- src/main/java/cmod/repz/application/service/module/RepzDiscordModule.java
+++ src/main/java/cmod/repz/application/service/module/RepzDiscordModule.java
@@ -1,22 +1,27 @@
 package cmod.repz.application.service.module;
 
 import cmod.repz.application.annotation.DiscordListenerComponent;
+import cmod.repz.application.service.DiscordDelayedMessageRemoverService;
 import cmod.repz.application.service.RepzRandomResponse;
 import cmod.repz.application.service.listener.DiscordCommandListener;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 @DiscordListenerComponent(command = "repz", description = "checks bot")
 @Slf4j
 public class RepzDiscordModule implements DiscordCommandListener {
     private final RepzRandomResponse repzRandomResponse;
+    private final DiscordDelayedMessageRemoverService discordDelayedMessageRemoverService;
 
-    public RepzDiscordModule(RepzRandomResponse repzRandomResponse) {
+    public RepzDiscordModule(RepzRandomResponse repzRandomResponse, DiscordDelayedMessageRemoverService discordDelayedMessageRemoverService) {
         this.repzRandomResponse = repzRandomResponse;
+        this.discordDelayedMessageRemoverService = discordDelayedMessageRemoverService;
     }
 
     @Override
     public void onCommand(GuildMessageReceivedEvent event, String[] args) {
-        event.getMessage().getChannel().sendMessage(repzRandomResponse.getRandomResponse()).complete();
+        Message message = event.getMessage().getChannel().sendMessage(repzRandomResponse.getRandomResponse()).complete();
+        discordDelayedMessageRemoverService.scheduleRemove(message, 120);
     }
 }
