a61a833c2d819d4f2254900828c5c576c4c25bd4
sepehr-gh <sepehrgh01@gmail.com>
2020-12-03 13:02:23 +0330

    Added !clean command to clean messages in a channel

--- src/main/java/cmod/repz/application/service/module/MemberAccessDiscordModule.java
+++ src/main/java/cmod/repz/application/service/module/MemberAccessDiscordModule.java
@@ -7,6 +7,8 @@ import cmod.repz.application.service.listener.DiscordMessageListener;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.util.Objects;
+
 @DiscordListenerComponent
 public class MemberAccessDiscordModule implements DiscordMessageListener {
     private final ConfigModel configModel;
@@ -20,11 +22,10 @@ public class MemberAccessDiscordModule implements DiscordMessageListener {
     @Override
     public void onMessage(GuildMessageReceivedEvent event) {
         if (event.getChannel().getId().equals(configModel.getDiscord().getChannels().get("access-grant"))) {
-            discordDelayedMessageRemoverService.scheduleRemove(event.getMessage(), 60); //todo: 60 -> 10
+            discordDelayedMessageRemoverService.scheduleRemove(event.getMessage(), 2); //todo: 60 -> 10
             if(event.getMessage().getContentRaw().toLowerCase().equals(configModel.getDiscord().getCustom().get("accept-rules-message").toLowerCase())){
                 Role rzmember = event.getJDA().getRoleById(configModel.getDiscord().getRoles().get("rzmember"));
-                event.getGuild().addRoleToMember(event.getMember(), rzmember).complete();
-                discordDelayedMessageRemoverService.scheduleRemove(event.getChannel().sendMessage("Welcome :)").complete(), 60);
+                event.getGuild().addRoleToMember(Objects.requireNonNull(event.getMember()), Objects.requireNonNull(rzmember)).complete();
             }
         }
     }
