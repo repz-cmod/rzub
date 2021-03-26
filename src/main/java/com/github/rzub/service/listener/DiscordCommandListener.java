622ce92cb5d43bbaef122e9a728a8e77602ef6ad
sepehr-gh <sepehrgh01@gmail.com>
2021-03-26 16:07:28 +0430

    feat: default implementation of getCommandId

--- src/main/java/com/github/rzub/service/listener/DiscordCommandListener.java
+++ src/main/java/com/github/rzub/service/listener/DiscordCommandListener.java
@@ -1,8 +1,15 @@
 package com.github.rzub.service.listener;
 
+import com.github.rzub.annotation.DiscordListenerComponent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public interface DiscordCommandListener {
     void onCommand(GuildMessageReceivedEvent event, String[] args);
-    String getCommandId();
+    default String getCommandId(){
+        DiscordListenerComponent discordListenerComponent = this.getClass().getAnnotation(DiscordListenerComponent.class);
+        if(discordListenerComponent == null)
+            throw new RuntimeException("@DiscordListenerComponent is not used on " + this.getClass().getName());
+
+        return discordListenerComponent.command();
+    }
 }
